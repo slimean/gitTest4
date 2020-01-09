bc4844d32552aba48fa25c494732f66065030e3e
Emil Juntunen <emil.juntunen@learnet.se>
2020-01-09 13:31:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitTest4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
